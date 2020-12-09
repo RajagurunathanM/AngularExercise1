f95c59a0815114e184a1157872cbd2afb1ad90d6
RajagurunathanM <rajagurunathan.1702091@srit.org>
2020-12-09 23:14:51 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit,  VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+ngOnInit(): void {
+
+  alert('Click the components tab to see the Magic');
+
+}
   name = 'Angular ' + VERSION.major; 
 }
