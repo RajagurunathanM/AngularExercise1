03fb9d4d0f67537a6ad86fd1ac43befce75f1944
RajagurunathanM <rajagurunathan.1702091@srit.org>
2020-12-10 10:11:57 +0530

    1 file modified

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
     selector:'dashboard',
     template:`
     <div style="background-image: linear-gradient(black); color:red; text-align:center">
-      <h1>Click the navbar components to see the magic</h1>
+      <h1>Click the navbar components to see the Angular magic</h1>
       <a href="https://angular.io/" ><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwyLY32o0R1ZXcNbDnGm1OyDe8HPoTsMB0ow&usqp=CAU' width=70% height=70% ></a>
     </div>
     `,
